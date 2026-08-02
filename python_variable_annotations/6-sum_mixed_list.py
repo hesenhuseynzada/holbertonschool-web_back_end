44b5238f838a36dcafc7a8d0f86fe0d51d666158
hesen <hesen123@gmail.com>
2026-08-02 12:36:29 +0400

    sc

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""List of mixed types"""
-from typing import List
+""" Mixed list """
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_list: List[int, float]) -> float:
-    """function sum_mixed_list"""
-    return sum(mxd_list)
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ function sum_mixed_list """
+    return sum(mxd_lst)
