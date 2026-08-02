db19c8d87b0cb3a2a4333dd8ed5ff0b0ae7cda9b
hesen <hesen123@gmail.com>
2026-08-02 12:35:53 +0400

    cs

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""List of mixed types"""
+from typing import List
+
+
+def sum_mixed_list(mxd_list: List[int, float]) -> float:
+    """function sum_mixed_list"""
+    return sum(mxd_list)
