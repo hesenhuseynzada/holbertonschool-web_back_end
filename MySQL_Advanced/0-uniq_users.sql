c4a4aa2113b11709f4f8b9da99839c16d02c8f33
hesen <hesen123@gmail.com>
2026-08-23 15:41:45 +0400

    ef

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,7 @@
+'''CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows'''
 CREATE database IF NOT EXISTS hbtn_0d_tvshows;
 
+'''Create table named users with three input'''
 CREATE TABLE IF NOT EXISTS users(
     id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email string(255) NOT NULL UNIQUE,
---
+++ MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+mysql uniq user database workstation
