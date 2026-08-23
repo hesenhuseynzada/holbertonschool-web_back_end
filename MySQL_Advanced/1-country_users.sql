88d3e61f286ebdac375b684253c7859d9dd96a84
hesen <hesen123@gmail.com>
2026-08-23 15:48:35 +0400

    j

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
---create table
+-- create users table SQL, add country column
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL
+    country ENUM ('US', 'CO', 'TN') NOT NULL
 );
