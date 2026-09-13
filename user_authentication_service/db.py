4688b46ec039c250df2f0edd80d1b9ecae933e82
hesen <hesen123@gmail.com>
2026-09-13 21:32:49 +0400

    rg

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python3
-
-"""DB module """
+''' Module data base '''
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from user import Base
+from user import Base, User
+from typing import TypeVar
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
-    """DB class
-    """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
+        ''' def init '''
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        ''' def session '''
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -36,3 +32,23 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kargs) -> TypeVar('User'):
+        ''' def find user '''
+        try:
+            user = self._session.query(User).filter_by(**kargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        ''' def update user '''
+        user = self.find_user_by(id=user_id)
+        for x, y in kwargs.items():
+            if hasattr(user, x):
+                setattr(user, x, y)
+            else:
+                raise ValueError
+        self._session.commit()
