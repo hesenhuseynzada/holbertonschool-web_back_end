a03a9da15b1924121d77ab4c565802f392954100
hesen <hesen123@gmail.com>
2026-09-20 17:44:05 +0400

    sc

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -15,7 +15,6 @@ users = {
 }
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
@@ -38,23 +37,6 @@ def index() -> str:
     return render_template('6-index.html')
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """ Determines best match for supported languages """
-    # check if there is a locale parameter/query string
-    if request.args.get('locale'):
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
-    # check if there is a locale in an existing user's profile
-    elif g.user and g.user.get('locale')\
-            and g.user.get('locale') in app.config['LANGUAGES']:
-        return g.user.get('locale')
-    # default to return as a failsafe
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 def get_user() -> Union[dict, None]:
     """ Returns user dict if ID can be found """
     if request.args.get('login_as'):
@@ -62,14 +44,29 @@ def get_user() -> Union[dict, None]:
         user = int(request.args.get('login_as'))
         if user in users:
             return users.get(user)
-    else:
-        return None
+    return None
 
 
 @app.before_request
 def before_request():
     """ Finds user and sets as global on flask.g.user """
     g.user = get_user()
+
+
+def get_locale() -> str:
+    """ Determines best match for supported languages """
+    # check if there is a locale parameter/query string
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    # check if there is a locale in an existing user's profile
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    # default to return as a failsafe
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
 
 
 if __name__ == "__main__":
